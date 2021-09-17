f8e93d1721b0903f2ca1d97e91f2609cbf0afce7
nik0rai <nikolay-sviridov@inbox.ru>
2021-09-17 18:44:52 +0300

    Pretty print of tree

---
+++ 1.txt/3)/Node.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zadanie1
+{
+    class Node
+    {
+        private Node parent = null;
+        private string Text;
+        private readonly List<Node> Children = new();
+
+        public Node(string _Text) => Text = _Text;
+        private void SetParent(Node _parent) => parent = _parent;
+        public void SetText(string _Text) => Text = _Text;
+
+        public string GetText() => Text;
+        public List<Node> GetChildren() => Children;
+
+        public void AddChild(Node child)
+        {
+            child.SetParent(this);
+            this.Children.Add(child);
+        }
+        public void AddChild(string _text)
+        {
+            Node newChild = new Node(_text);
+            this.AddChild(newChild);
+        }
+        public void AddChildren(List<Node> children)
+        {
+            foreach (Node i in children)
+                i.SetParent(this);
+
+            this.Children.AddRange(children);
+        }
+       
+        public void Print(string pref, bool last)
+        {
+
+            Console.Write(pref);
+            if (last){
+                Console.Write("└─");
+                pref += "  ";
+            }
+            else{
+                Console.Write("├─");
+                pref += "| ";
+            }
+            Console.WriteLine(Text);
+
+
+            for (int i = 0; i < Children.Count; i++)
+                Children[i].Print(pref, i == Children.Count - 1);
+        }
+    }
+}
---
+++ 1.txt/3)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node root = new("root");
+            Node child1 = new("child1");
+            child1.AddChild("grandchild1");
+            child1.AddChild("grandchild2");
+
+            Node child2 = new("child2");
+            child2.AddChild("grandchild3");
+
+            root.AddChild(child1);
+            root.AddChild(child2);
+
+            root.GetChildren()[0].GetChildren()[1].AddChild("grandgrandchild1(connected to 2nd)");
+
+
+            List<Node> ch = new();
+            ch.Add(new Node("Child4"));
+            ch.Add(new Node("Child5"));
+            ch.Add(new Node("Child6"));
+            root.AddChildren(ch);
+
+            //strings: ├ │ ─
+            root.Print("", true);
+        }
+    }
+
+}
