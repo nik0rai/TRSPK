bbabee81937a015bb98feb35c2406138a06f9866
nik0rai <nikolay-sviridov@inbox.ru>
2021-10-01 18:39:33 +0300

    Update ArrEl.cs

--- 3.txt/3)/ArrEl.cs
+++ 3.txt/3)/ArrEl.cs
@@ -1,9 +1,17 @@
-class ArrEl<TKey, TValue>
+ class ArrEl<TKey, TValue>
     {
         private readonly List<Item<TKey, TValue>> _items = new();
-        public int Count => _items.Count; //кол-во элементов
+
+        /// <returns>Кол-во элементов.</returns>
+        public int Count => _items.Count;
         public IReadOnlyList<TKey> Keys => (IReadOnlyList<TKey>)_items.Select(i => i.Key).ToList(); //коллекция ключей
 
+        /// <summary>
+        /// Добавляет пару ключ и значение.
+        /// </summary>
+        /// <param name="item">Ключ со значением.</param>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="ArgumentNullException"></exception>
         public void Add(Item<TKey, TValue> item)
         {
             if (item == null) throw new ArgumentNullException(nameof(item));
@@ -13,6 +21,13 @@ class ArrEl<TKey, TValue>
             _items.Add(item); //добавляем данные
         }
 
+        /// <summary>
+        /// Добавляет пару ключ и значение.
+        /// </summary>
+        /// <param name="key">Ключ.</param>
+        /// <param name="value">Значение.</param>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="ArgumentNullException"></exception>
         public void Add(TKey key, TValue value)
         {
             if (key == null) throw new ArgumentNullException(nameof(key));
@@ -31,6 +46,11 @@ class ArrEl<TKey, TValue>
             _items.Add(item); //добавляем данные 
         }
 
+        /// <summary>
+        /// Удаляет пару ключ и значение по ключу.
+        /// </summary>
+        /// <param name="key">Ключ.</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public void Delete(TKey key)
         {
             if (key == null) throw new ArgumentNullException(nameof(key));
@@ -42,6 +62,13 @@ class ArrEl<TKey, TValue>
             if (item != null) _items.Remove(item);
         }
 
+        /// <summary>
+        /// Получает значение по ключу.
+        /// </summary>
+        /// <param name="key">Ключ.</param>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <returns>Значение</returns>
         public TValue Get(TKey key)
         {           
             if (key == null) throw new ArgumentNullException(nameof(key));
@@ -51,7 +78,14 @@ class ArrEl<TKey, TValue>
 
             return item.Value;
         }
-        
+
+        /// <summary>
+        /// Получает ключ по значению.
+        /// </summary>
+        /// <param name="val">Значение.</param>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <returns>Ключ</returns>
         public TKey Get(TValue val)
         {
             if (val == null) throw new ArgumentNullException(nameof(val));
