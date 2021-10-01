2593f27aa8c9baf23e673380b48f1d9ddc8ebd55
Arina Nikitina <71166058+arinanikitina@users.noreply.github.com>
2021-10-01 20:04:06 +0300

    git commit -m

---
+++ 3.txt/2)/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+//Добавить в классы свойства Имя, Возраст, Школа, Место работы, и реализовать их параллельно методам изменения полей
+namespace Man
+{
+    class Man
+    {
+        public uint age;
+        public string name;
+        public string Name
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException();
+                }
+                else
+                {
+                    name = value;
+                }
+            }
+            get { return name; }
+        }
+    
+        public uint Age { get; set; }
+
+        public void changeName (string name)
+        {
+            Name = name;
+            if (string.IsNullOrEmpty(name)) throw new ArgumentNullException(); //Исключение: поле не может быть пустым
+        }
+
+        public virtual void changeAge (uint age)
+        {
+            Age = age;
+        }
+
+        public virtual string Display() => (nameof(Man) + ' ' + Name + ' ' + Age); //Пара virtual-override для использования метода в других классах с учетом изменений
+    };
+    class Teenager : Man // наследник класса Man 
+    {
+        public string School { get; set; }
+        new public uint Age
+        {
+            set
+            {
+                if (value < 13 || value > 19)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+            get { return age; }
+        }
+
+        public override void changeAge(uint age)
+        {
+            Age = age;
+            if (age < 13 || age > 19) throw new ArgumentOutOfRangeException(); //Исключение: выход за диапазон допустимых значений
+        }
+
+        public override string Display() => (nameof(Teenager) + ' ' + Name + ' ' + Age + " Place of study: " + School); //Пара virtual-override для использования метода в других классах с учетом изменений
+
+    };
+    class Worker : Man
+    {
+        public string Workplace { get; set; }
+
+        new public uint Age
+        {
+            set
+            {
+                if (value < 16 || value > 70)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+            get { return age; }
+        }
+
+        public override string Display() => (nameof(Worker) + ' ' + Name + ' ' + Age + " Place of work: " + Workplace); //Пара virtual-override для использования метода в других классах с учетом изменений
+    }; 
+}
