3dd63a72bfeb1af3c774259fca05236b8493590c
mrtemnikova <71551212+mrtemnikova@users.noreply.github.com>
2021-10-01 16:41:54 +0300

    ref

---
+++ 3.txt/5)/3.5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Sw
+    {
+        public static void Swap(ref int x, ref int y)
+        {
+            (x, y) = (y, x);
+        }
+    }
+    class Program
+    {  
+        static void Main()
+        {    
+            int x = 1;
+            int y = 0;
+            Console.WriteLine($"{ x}, { y}"); // 1 0
+            Sw.Swap(ref x, ref y);
+            Console.WriteLine($"{ x}, { y}"); // 0 1
+
+        }
+    }
+}
