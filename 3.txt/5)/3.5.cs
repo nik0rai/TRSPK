89399a1ca2ec761da7379160a91d9e94231a03ae
nik0rai <nikolay-sviridov@inbox.ru>
2021-10-02 13:01:29 +0300

    Update 3.5.cs

--- 3.txt/5)/3.5.cs
+++ 3.txt/5)/3.5.cs
@@ -1,22 +1,13 @@
-﻿using System;
-
-namespace ConsoleApp6
-{
-    class Sw
-    {
-        public static void Swap(ref int x, ref int y)
-        {
-            (x, y) = (y, x);
-        }
-    }
     class Program
     {  
+        public static void Swap(ref int x, ref int y) =>  (x, y) = (y, x);         
+        
         static void Main()
         {    
             int x = 1;
             int y = 0;
             Console.WriteLine($"{ x}, { y}"); // 1 0
-            Sw.Swap(ref x, ref y);
+            Swap(ref x, ref y);
             Console.WriteLine($"{ x}, { y}"); // 0 1
 
         }
