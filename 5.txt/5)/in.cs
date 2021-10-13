e862df9b6bf33834d72481bdddd43221dba636f3
mrtemnikova <71551212+mrtemnikova@users.noreply.github.com>
2021-10-13 13:37:05 +0300

    Add files via upload

---
+++ 5.txt/5)/in.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Diagnostics;
+
+namespace Intern
+{
+    static class Programm
+    {
+        static void Main(string[] args)
+        {
+
+            var a = "aasd fghjk";
+            var b = "qawe rty";
+
+            bool equals = false;
+            var stopwatch = Stopwatch.StartNew();
+            for (int i = 0; i <Math.Max(a.Length,b.Length) ; i++)
+            {
+                if (a == b) equals=true;
+            }
+
+            stopwatch.Stop();
+            string.Intern("aasdf ghjk");
+            string.Intern("qawe rty");
+            equals = false;
+            var stopwatch1 = Stopwatch.StartNew();
+            for (int i = 0; i < Math.Max(a.Length, b.Length); i++)
+            {
+                if (a == b) equals = true;
+            }
+            stopwatch1.Stop();
+            Console.WriteLine(stopwatch.Elapsed.TotalMilliseconds + " - " + equals);
+            Console.WriteLine(stopwatch1.Elapsed.TotalMilliseconds + " - " + equals);
+            Console.WriteLine(stopwatch.Elapsed.TotalMilliseconds/stopwatch1.Elapsed.TotalMilliseconds);
+        }
+
+
+    }
+}
