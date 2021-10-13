699d7aaaccd49f0d35f989833dfaac6b98143627
Arina Nikitina <71166058+arinanikitina@users.noreply.github.com>
2021-10-13 13:18:51 +0300

    git commit

---
+++ 5.txt/4)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+class HelloWorld
+{
+    static void Main()
+    {
+
+        Console.WriteLine("text: hello world");
+
+        Stopwatch stopwatch = new();
+
+        string s1 = "hellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohello";
+        string s2 = "worldworldworldworldworldworldworldworldworldworldworldworldworldworldworldworldworldworldworld";
+
+        StringBuilder sb = new();
+
+        Console.WriteLine("String: ");
+        stopwatch.Start();
+        _ = s1 + ", " + s2;
+        stopwatch.Stop();
+        Console.WriteLine("Elapsed time: {0}ms", stopwatch.Elapsed.TotalMilliseconds);
+
+        Console.WriteLine("StringBuilder: ");
+        stopwatch.Start();
+        sb.AppendLine(s1 + ", " + s2);
+        stopwatch.Stop();
+        Console.WriteLine("Elapsed time: {0}ms", stopwatch.Elapsed.TotalMilliseconds);
+
+
+    }
+}
