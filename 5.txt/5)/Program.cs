3db0109974860c22d494cb601f6e54247a33f56c
Arina Nikitina <71166058+arinanikitina@users.noreply.github.com>
2021-10-13 12:31:12 +0300

    git commit

---
+++ 5.txt/5)/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Diagnostics;
+class HelloWorld
+{
+    static void Main()
+    {
+
+        Stopwatch stopwatch = new();
+        bool check = true;
+        int a = 8;
+        int i;
+
+        stopwatch.Start();
+        for (i = 0; i < 10; i++)
+        {
+            if (a == 8)
+            {
+                check = true;
+                i++;
+            }
+        }
+        stopwatch.Stop();
+        Console.WriteLine("Elapsed time: {0}ms", stopwatch.Elapsed.TotalMilliseconds);
+
+        stopwatch.Start();
+            if (a == 8) check = true;
+        stopwatch.Stop();
+        Console.WriteLine("Elapsed time: {0}ms", stopwatch.Elapsed.TotalMilliseconds);
+
+
+    }
+}
