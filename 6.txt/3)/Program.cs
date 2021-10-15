bec94c0e64061e36f088920e23ef63056f238152
Arina Nikitina <71166058+arinanikitina@users.noreply.github.com>
2021-10-15 18:59:44 +0300

    git commit

---
+++ 6.txt/3)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace enumchik
+{
+    class Program
+    {
+        enum Days
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int i = Convert.ToInt32(Console.ReadLine());
+            i--;
+            Console.WriteLine(Enum.GetName(typeof(Days), i));
+        }
+    }
+}
