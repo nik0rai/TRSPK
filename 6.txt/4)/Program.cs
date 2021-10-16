809a1aedc48ab6c719f6e86e9348d5188b14ea9c
McGrath120 <90923806+McGrath120@users.noreply.github.com>
2021-10-16 12:30:57 +0300

    Add files via upload

---
+++ 6.txt/4)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _6._4
+{
+    [Flags]
+    public enum Group
+    {
+        Arina = 1 << 0,
+        Marina = 1 << 1,
+        Nikolay = 1 << 2,
+        MarkWolf = 1 << 3,
+        Anna = 1 << 4
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var enums = (Group.Arina | Group.Marina | Group.Nikolay |
+                         Group.MarkWolf | Group.Anna).ToString();
+            Console.WriteLine(enums);
+
+            Console.WriteLine(Enum.Parse(typeof(Group), "31").ToString());      //31 = 2^5 - 2^0
+
+
+
+
+        }
+    }
+}
